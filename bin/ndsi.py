a3cf33748b9bff3bdc0508ace4125f91b4e2a086
Michelle <jmhu@uw.edu>
2019-04-30 14:11:17 -0700

    In-progress update

--- bin/ndsi.py
+++ bin/ndsi.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 
-# Calculate Normalized Difference Snow Index from DigitalGlobe data imagery
-# that are already aligned and orthorectified
+# Calculate Normalized Difference Snow Index from orthorectified, resampled, and clipped WV-3 top-of-atmosphere reflectance imagery and Level 1 Landsat 8 imagery.
+
 # Adapted from http://gencersumbul.bilkent.edu.tr/post/gdal_scripts/
 
 # import libraries
+from osgeo import gdalconst
 from gdalconst import *
 import argparse, numpy as np,  gdal, struct, sys
 
-# dictionary of data type conversion between gdal and numpy
+print(gdal.VersionInfo())
+
+# Have user define input and output image filenames
+parser = argparse.ArgumentParser(description='GeoTiff Multi Spectral Image to NDSI Image Conversion Script with Normalized Difference Snow Index Measurement')
+parser.add_argument('-in', '--MS_input_file', help='GeoTiff multi band MS image file', required=True)
+parser.add_argument('-in2', '--SWIR_input_file', help='GeoTiff multi band SWIR image file for WV3', required=False)
+parser.add_argument('-in_sensor', '--input_satellite', help='Sensor name - either WV3 or L8', required=True)
+parser.add_argument('-in_ndsi', '--input_thresh', help='String of NDSI outfile type: either base or hall', required=False)
+parser.add_argument('-out', '--output_file', help='Where NDSI image is to be saved', required=True)
+args = parser.parse_args()
+
+multi_band_file = args.MS_input_file
+swir_file = args.SWIR_input_file
+sensor = args.input_satellite
+NDSI_type = args.input_thresh
+NDSI_file = args.output_file
+
+# Dictionary of data type conversions between gdal and numpy
 GDAL2NUMPY_DATA_TYPE_CONVERSION = {
   1: "uint8",
   2: "uint16",
@@ -21,52 +39,44 @@ GDAL2NUMPY_DATA_TYPE_CONVERSION = {
   11: "complex128",
 }
 
-# Have user define multi-band raster and output filename
-parser = argparse.ArgumentParser(description='GeoTiff Multi Spectral Image to NDVI Image Conversion Script with Normalized Difference Vegetation Index Measurement')
-parser.add_argument('-in', '--input_file', help='GeoTiff multi band MS image file', required=True)
-parser.add_argument('-in_sensor', '--input_satellite', help='String of satellite', required=True)
-parser.add_argument('-in_ndsi', '--input_thresh', help='String of NDSI outfile', required=True)
-parser.add_argument('-out', '--output_file', help='Where NDSI image is to be saved', required=True)
-args = parser.parse_args()
 
-multi_band_file = args.input_file
-sensor = args.input_satellite
-NDSI_type = args.input_thresh
-NDSI_file = args.output_file
-
-
-#Open multi-band file as general access read only
-multi_band_dataset = gdal.Open(multi_band_file, GA_ReadOnly)
-print(multi_band_file, "Driver:", multi_band_dataset.GetDriver().ShortName, "/", multi_band_dataset.GetDriver().LongName)
-print(multi_band_file, "Size:", multi_band_dataset.RasterXSize, "x", multi_band_dataset.RasterYSize, "x", multi_band_dataset.RasterCount)
-
-
-# With L8 multiband image from 12.5.18, this is rearranged as in WV3
-# green=1
-# swir=4
-
-# Extract the green, NIR, and SWIR bands, dictate dn_range from WV3 and L8 imagery
-# Need to fix the worldview stuff for extraction from two different multi or single band files
+# Extract the green, NIR, and SWIR bands. Dictate dn_range for WV3 and L8 imagery (should be TOA reflectance values)
 if sensor == 'WV3':
-    green = 1
-    nir = 3
-    swir = 4
-    dn_range = 1#2047
+    ms_noext = multi_band_file[:-4]
+    swir_noext = swir_file[:-4]
+
+    # Open single-band files as general access read only
+    multi_band_dataset = gdal.Open(ms_noext + "_b3_toa_refl.tif", GA_ReadOnly)
+    green_band = multi_band_dataset.GetRasterBand(1)
+    print(green_band.XSize)
+    nir_band = gdal.Open(ms_noext + "_b7_toa_refl.tif", GA_ReadOnly).GetRasterBand(1)
+    swir_band = gdal.Open(swir_noext + "_b3_toa_refl.tif", GA_ReadOnly).GetRasterBand(1)
+    dn_range = 1    
+    
 else:
+    # L8 sensor level 1 imagery
     green = 3
     nir = 5
     swir = 6
-    dn_range = 1#4095
+    dn_range = 1
+
+    # Open single-band files as general access read only
+    multi_band_dataset = gdal.Open(multi_band_file, GA_ReadOnly)  
+    green_band = multi_band_dataset.GetRasterBand(green)
+    nir_band = multi_band_dataset.GetRasterBand(nir)
+    swir_band = multi_band_dataset.GetRasterBand(swir)
 
 
-green_band = multi_band_dataset.GetRasterBand(green)
-nir_band = multi_band_dataset.GetRasterBand(nir)
-swir_band = multi_band_dataset.GetRasterBand(swir)
+# Print out general information on dataset - choose green band
+print(multi_band_file, "Driver:", multi_band_dataset.GetDriver().ShortName, 
+      "/", multi_band_dataset.GetDriver().LongName)
+print(multi_band_file, "Size:", multi_band_dataset.RasterXSize, "x", 
+      multi_band_dataset.RasterYSize, "x", multi_band_dataset.RasterCount)
 
 xsize = green_band.XSize
 ysize = green_band.YSize
 
-#Create NVDI output raster with specific raster format
+#Create NDSI output raster with specific raster format
 driver = gdal.GetDriverByName('GTiff')
 
 NDSI_dataset = driver.Create(
@@ -74,17 +84,19 @@ NDSI_dataset = driver.Create(
     multi_band_dataset.RasterXSize,
     multi_band_dataset.RasterYSize,
     1, # number of output bands -- just need one for NDSI
-    6,) #float32
+    6,) # float32
 
 # Match the geotransform and projection to that of the input image
 NDSI_dataset.SetGeoTransform(multi_band_dataset.GetGeoTransform())
 NDSI_dataset.SetProjection(multi_band_dataset.GetProjection())
+# NDSI_dataset.GetRasterBand(1).SetNoDataValue(-32768)
 
 # Populate NDSI raster, use data blocks to save on memory usage
-block_sizes = green_band.GetBlockSize()
-x_block_size = block_sizes[0]
-y_block_size = block_sizes[1]
-
+#block_sizes = green_band.GetBlockSize()
+    # Set to 1024 x 1024 - when gdalwarping WV3 imagery in previous steps to get to toa_refl, these are getting messed around along the way...
+x_block_size = 1024 #block_sizes[0]
+y_block_size = 1024 #block_sizes[1]
+    
 blocks = 0
 
 # loop through rows
@@ -93,30 +105,39 @@ for y in range(0, ysize, y_block_size):
         rows = y_block_size
     else:
         rows = ysize - y
+
+    print(rows)
     # Loop through columns
     for x in range(0, xsize, x_block_size):
         if x + x_block_size < xsize:
             cols = x_block_size
         else:
             cols = xsize - x
+        # Read GDAL dataset as numpy array of specified type
         green_band_array = green_band.ReadAsArray(x, y, cols, rows).astype('float32')
+        print(type(nir_band))
         nir_band_array = nir_band.ReadAsArray(x, y, cols, rows).astype('float32')
+        print(type(nir_band_array))
         swir_band_array = swir_band.ReadAsArray(x, y, cols, rows).astype('float32')
+        
+        # Omit pixels with negative reflectance values or reflectance values > 1
+        green_band_array = (green_band_array >= 0) & (green_band_array <=1)
+        nir_band_array = (nir_band_array >= 0) & (nir_band_array <=1)
+        swir_band_array = (swir_band_array >= 0) & (swir_band_array <=1)
 
         # Mask values which are undefined due to zero division (Green + SWIR = 0)
         mask_array = np.not_equal((green_band_array + swir_band_array), 0)
-        # Calculate NDSI values
-        ndsi_array = np.choose(mask_array, (-32768, (green_band_array - swir_band_array) / (swir_band_array + green_band_array)))
+        
+        # Calculate NDSI values and put into separate array
+        ndsi_array = np.choose(mask_array, (-32768, (green_band_array - swir_band_array) / (green_band_array + swir_band_array)))
 
-        # implement threshold based on nir and green values
-        ndsi_thresh_array = np.where((ndsi_array >= 0.4) & (ndsi_array <= 1.0) & (nir_band_array>=0.1 * dn_range) & (green_band_array>=0.1 * dn_range), ndsi_array, -32768)
-        # If we implement a max threshold on the vis bands, the brightest snow areas are omitted --> will have to reclassify values above 1 to 1 in reflectance or figure something out. 12.24.18
-        # ndsi_thresh_array = np.where((ndsi_array >= 0.4) & (ndsi_array <= 1.0) & (nir_band_array>=0.1 * dn_range) & (nir_band_array<=1.0 * dn_range) & (green_band_array>=0.1 * dn_range) & (green_band_array<=1.0 * dn_range), ndsi_array, -32768)
-
-        if NDSI_type == "thresh":
-            NDSI_dataset.GetRasterBand(1).WriteArray(ndsi_thresh_array.astype('float32'), x, y)
-        else:
-            NDSI_dataset.GetRasterBand(1).WriteArray(ndsi_array.astype('float32'), x, y)
+        # Adjust NDSI values based on threshold method
+        if NDSI_type == "hall":
+            # implement threshold based on nir and green values
+            ndsi_array = np.where((ndsi_array >= 0.4) & (ndsi_array <= 1.0) & (nir_band_array>=0.1 * dn_range) & (green_band_array>=0.1 * dn_range), ndsi_array, -32768)
+        
+        NDSI_dataset.GetRasterBand(1).WriteArray(ndsi_array.astype('float32'), x, y)
+#         print(ndsi_array.shape, type(ndsi_array), ndsi_array.min(), ndsi_array.max())
 
 
         del green_band_array
