a88d36b695b8d8f2e8e9b6bf390963d955a3b737
Michelle <jmhu@uw.edu>
2019-04-30 21:51:30 -0700

    Clean up formatting

--- bin/ndvi.py
+++ bin/ndvi.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python
 
-# Calculate NDVI from input imagery that is already aligned and orthorectified
+# Calculate Normalized Difference Snow Index from orthorectified, resampled, and clipped WV-3 top-of-atmosphere reflectance imagery, Landsat 8 Level 1 imagery, and Planet Level 3B SR imagery.
+
 # Adapted from http://gencersumbul.bilkent.edu.tr/post/gdal_scripts/
 
+# system libraries and imports
+import sys
+import argparse
+import struct
 
-# import libraries
-from osgeo import gdalconst
-from gdalconst import *
-import argparse, numpy as np, gdal, struct, sys
+# import third party
+import numpy as np
+from osgeo import gdal
+
+# Have user define input and output image filenames
+parser = argparse.ArgumentParser(description='GeoTiff Multi Spectral Image to NDVI Image Conversion Script with Normalized Difference Vegetation Index Measurement')
+parser.add_argument('-in', '--input_file', help='Multiband MS image file', required=True)
+parser.add_argument('-in_sensor', '--input_satellite', help='Sensor name - either WV3 or L8', required=True)
+parser.add_argument('-out', '--output_file', help='Where NDVI image is to be saved', required=True)
+args = parser.parse_args()
+
+multi_band_file = args.input_file
+sensor = args.input_satellite
+NDVI_file = args.output_file
 
 # dictionary of data type conversion between gdal and numpy
 GDAL2NUMPY_DATA_TYPE_CONVERSION = {
@@ -22,42 +37,52 @@ GDAL2NUMPY_DATA_TYPE_CONVERSION = {
   11: "complex128",
 }
 
-# Have user define multi-band raster and output filename
-parser = argparse.ArgumentParser(description='GeoTiff Multi Spectral Image to NDVI Image Conversion Script with Normalized Difference Vegetation Index Measurement')
-parser.add_argument('-in', '--input_file', help='GeoTiff multi band image file', required=True)
-parser.add_argument('-in_sensor', '--input_satellite', help='String of satellite', required=True)
-parser.add_argument('-out', '--output_file', help='Where NDVI image is to be saved', required=True)
-args = parser.parse_args()
-
-multi_band_file = args.input_file
-sensor = args.input_satellite
-NDVI_file = args.output_file
-
-
-#Open multi-band file as general access read only
-multi_band_dataset = gdal.Open(multi_band_file, GA_ReadOnly)
-print(multi_band_file, "Driver:", multi_band_dataset.GetDriver().ShortName, "/", multi_band_dataset.GetDriver().LongName)
-print(multi_band_file, "Size:", multi_band_dataset.RasterXSize, "x", multi_band_dataset.RasterYSize, "x", multi_band_dataset.RasterCount)
-
-#Extract the red and near-infrared bands and sizes the input imagery
-
+# Extract the red and NIR bands (should be TOA reflectance values)
 if sensor == 'WV3':
-    red = 2
-    nir = 3
-elif sensor == 'Planet':
-    red = 3
-    nir = 4
+    # Remove file extension
+    ms_noext = multi_band_file[:-4]
+    
+    # Open single-band files as general access read only
+    multi_band_dataset = gdal.Open(ms_noext + "_b5_toa_refl.tif", gdal.GA_ReadOnly)
+    red_band = multi_band_dataset.GetRasterBand(1)
+    
+    nir_dataset = gdal.Open(ms_noext + "_b7_toa_refl.tif", gdal.GA_ReadOnly)
+    nir_band = nir_dataset.GetRasterBand(1)
+
 else:
-    red = 4
-    nir = 5
+    if sensor == 'Planet':
+        red = 3
+        nir = 4
+    else:
+        # L8 sensor level 1 imagery
+        red = 4
+        nir = 5
 
-red_band = multi_band_dataset.GetRasterBand(red)
-infrared_band = multi_band_dataset.GetRasterBand(nir)
+    # Open single-band files as general access read only
+    multi_band_dataset = gdal.Open(multi_band_file, gdal.GA_ReadOnly)  
+    red_band = multi_band_dataset.GetRasterBand(red)
+    nir_band = multi_band_dataset.GetRasterBand(nir)
 
+# Print out general information on dataset
+print(multi_band_file, 
+      "Driver:", multi_band_dataset.GetDriver().ShortName, 
+      "/", multi_band_dataset.GetDriver().LongName)
+print(multi_band_file, 
+      "Size:", multi_band_dataset.RasterXSize, 
+      "x", multi_band_dataset.RasterYSize, 
+      "x", multi_band_dataset.RasterCount)
+
+# Extract rows and columns of bands
 xsize = red_band.XSize
 ysize = red_band.YSize
 
-#Create NVDI output raster with specific raster format
+# Populate NDSI raster, use data blocks to save on memory usage
+#block_sizes = green_band.GetBlockSize()
+# Set to 1024 x 1024 - when gdalwarping WV3 imagery in previous steps to get to toa_refl, these are getting messed around along the way because you are setting them in the options (see below)
+x_block_size = 1024 #block_sizes[0]
+y_block_size = 1024 #block_sizes[1]
+
+# Create NDVI output raster with specific raster format
 driver = gdal.GetDriverByName('GTiff')
 
 NDVI_dataset = driver.Create(
@@ -65,16 +90,20 @@ NDVI_dataset = driver.Create(
     multi_band_dataset.RasterXSize,
     multi_band_dataset.RasterYSize,
     1, # number of output bands -- just need one for NDVI
-    6,) #float32
+    6, #float32
+    options=['TILED=YES', 
+             'BLOCKXSIZE=%i' % x_block_size, 
+             'BLOCKYSIZE=%i' % y_block_size, 
+             'BIGTIFF=IF_SAFER', 
+             'COMPRESS=LZW',
+            ])
 
 # Match the geotransform and projection to that of the input image
 NDVI_dataset.SetGeoTransform(multi_band_dataset.GetGeoTransform())
 NDVI_dataset.SetProjection(multi_band_dataset.GetProjection())
 
-# Populate NDVI raster, use data blocks to save on memory usage
-block_sizes = red_band.GetBlockSize()
-x_block_size = block_sizes[0]
-y_block_size = block_sizes[1]
+ndvi_band_out = NDVI_dataset.GetRasterBand(1)
+ndvi_band_out.SetNoDataValue(-32768)
 
 blocks = 0
 
@@ -90,24 +119,28 @@ for y in range(0, ysize, y_block_size):
             cols = x_block_size
         else:
             cols = xsize - x
+        
+        # Read GDAL dataset as numpy array of specified type
         red_band_array = red_band.ReadAsArray(x, y, cols, rows).astype('float32')
-        infrared_band_array = infrared_band.ReadAsArray(x, y, cols, rows).astype('float32')
+        nir_band_array = nir_band.ReadAsArray(x, y, cols, rows).astype('float32')
 
-        # Mask values which are undefined due to zero division (NIR + Red = 0)
-        mask_array = np.not_equal((red_band_array + infrared_band_array), 0)
-        ndvi_array = np.choose(mask_array, (-32768, (infrared_band_array - red_band_array) / (infrared_band_array + red_band_array)))
-        NDVI_dataset.GetRasterBand(1).WriteArray(ndvi_array.astype('float32'), x, y)
+        # Calculate NDVI
+        ndvi_array = (nir_band_array - red_band_array) / (nir_band_array + red_band_array)
+        
+        # Create mask of valid pixels - positive reflectance values <=1
+        valid_mask = (red_band_array >= 0) & (red_band_array <=1) & (nir_band_array >= 0) & (nir_band_array <= 1)        
+        ndvi_array = np.ma.array(ndvi_array, mask=red_nir_mask, fill_value=-32768)
+        ndvi_band_out.WriteArray(ndvi_array, x, y)
 
-        del red_band_array
-        del infrared_band_array
-        del mask_array
-        del ndvi_array
+        red_band_array = None
+        nir_band_array = None
+        mask_array = None
+        ndvi_array = None
 
         blocks += 1
 
 # Set dataset and bands to None to clear memory usage
-
 red_band = None
-infrared_band = None
+nir_band = None
 multi_band_dataset = None
 NDVI_dataset = None
