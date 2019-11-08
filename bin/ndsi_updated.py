b69a7a4e42b23c04e2bd69f15b857014a1112d79
Michelle Hu <jmhu@uw.edu>
2019-11-07 18:40:24 -0800

    ndsi_updated.py: comment out bounded and ndsi_3 calculations.  Use NDSI_2 alone

--- bin/ndsi_updated.py
+++ bin/ndsi_updated.py
@@ -22,10 +22,10 @@ multi_band_file = args.MS_input_file
 swir_file = args.SWIR_input_file
 out_fn = args.output_file
 
-out_fn_2=out_fn[:-4]+"_s2.tif"
-out_fn_3=out_fn[:-4]+"_s3.tif"
-out_fn_2_adj=out_fn[:-4]+"_s2_bounded.tif"
-out_fn_3_adj=out_fn[:-4]+"_s3_bounded.tif"
+# out_fn_2=out_fn[:-4]+"_s2.tif"
+# out_fn_3=out_fn[:-4]+"_s3.tif"
+# out_fn_2_adj=out_fn[:-4]+"_s2_bounded.tif"
+# out_fn_3_adj=out_fn[:-4]+"_s3_bounded.tif"
 
 # Extract reflectance as numpy arrays from proper bands (TOA or SR)    
 with rio.open(multi_band_file[:-4] + "_b3_toa_refl.tif") as f:
@@ -47,13 +47,13 @@ print(g_ndv, swir2_ndv, swir3_ndv)
 # NDSI = (green - swir) / (green + swir)
 # In ndsi.py script, NDSI is calculated with values (0, 1] for both green and SWIR bands
 ndsi_2= (green_arr - swir2_arr) / (green_arr + swir2_arr)
-ndsi_3= (green_arr - swir3_arr) / (green_arr + swir3_arr)
+# ndsi_3= (green_arr - swir3_arr) / (green_arr + swir3_arr)
 
 # Mask with ndv areas from green and swir arrays
 ndsi_2[green_arr==g_ndv]=g_ndv
 ndsi_2[swir2_arr==swir2_ndv]=swir2_ndv
-ndsi_3[green_arr==g_ndv]=g_ndv
-ndsi_3[swir3_arr==swir3_ndv]=swir3_ndv
+# ndsi_3[green_arr==g_ndv]=g_ndv
+# ndsi_3[swir3_arr==swir3_ndv]=swir3_ndv
 
 # Write these to array
 with rio.Env():
@@ -64,33 +64,35 @@ with rio.Env():
         count=1,
         compress='lzw')
 
-    with rio.open(out_fn_2, 'w', **prf) as dst:
+    with rio.open(out_fn, 'w', **prf) as dst:
         dst.write(np.squeeze(ndsi_2).astype(rio.float32), 1)
 
-    with rio.open(out_fn_3, 'w', **prf) as dst:
-        dst.write(np.squeeze(ndsi_3).astype(rio.float32), 1)
-# Adjust with out of bounds green and swir reflectance values
-ndsi_2[green_arr<0]=g_ndv
-ndsi_2[swir2_arr<0]=swir2_ndv
-ndsi_2[green_arr>1]=g_ndv
-ndsi_2[swir2_arr>1]=swir2_ndv
+#     with rio.open(out_fn_3, 'w', **prf) as dst:
+#         dst.write(np.squeeze(ndsi_3).astype(rio.float32), 1)
+        
+# # Do not institute low and high bounds for initial reflectance values right now.
+# # Adjust with out of bounds green and swir reflectance values
+# ndsi_2[green_arr<0]=g_ndv
+# ndsi_2[swir2_arr<0]=swir2_ndv
+# ndsi_2[green_arr>1]=g_ndv
+# ndsi_2[swir2_arr>1]=swir2_ndv
 
-ndsi_3[green_arr<0]=g_ndv
-ndsi_3[swir3_arr<0]=swir3_ndv
-ndsi_3[green_arr>1]=g_ndv
-ndsi_3[swir3_arr>1]=swir3_ndv
+# ndsi_3[green_arr<0]=g_ndv
+# ndsi_3[swir3_arr<0]=swir3_ndv
+# ndsi_3[green_arr>1]=g_ndv
+# ndsi_3[swir3_arr>1]=swir3_ndv
 
-# And write to array
-with rio.Env():
-    # Change the band count to 1, set the
-    # dtype to float 32, and specify LZW compression.
-    prf.update(
-        dtype=rio.float32,
-        count=1,
-        compress='lzw')
+# # And write to array
+# with rio.Env():
+#     # Change the band count to 1, set the
+#     # dtype to float 32, and specify LZW compression.
+#     prf.update(
+#         dtype=rio.float32,
+#         count=1,
+#         compress='lzw')
 
-    with rio.open(out_fn_2_adj, 'w', **prf) as dst:
-        dst.write(np.squeeze(ndsi_2).astype(rio.float32), 1)
+#     with rio.open(out_fn_2_adj, 'w', **prf) as dst:
+#         dst.write(np.squeeze(ndsi_2).astype(rio.float32), 1)
 
-    with rio.open(out_fn_3_adj, 'w', **prf) as dst:
-        dst.write(np.squeeze(ndsi_3).astype(rio.float32), 1)
+#     with rio.open(out_fn_3_adj, 'w', **prf) as dst:
+#         dst.write(np.squeeze(ndsi_3).astype(rio.float32), 1)
