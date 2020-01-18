4117f6382593268dc0d4c9d0d611d2840f6910da
Michelle Hu <jmhu@uw.edu>
2020-01-17 21:21:59 -0800

    ndsi_updated.py: add pixel resolution handling and input filenames, standardize ndv, add minmax normalization

--- bin/ndsi_updated.py
+++ bin/ndsi_updated.py
@@ -13,11 +13,13 @@ import rasterio as rio
 
 # Have user define input and output image filenames
 parser = argparse.ArgumentParser(description='NDSI Calculation Script with Normalized Difference Snow Index Measurement')
-parser.add_argument('-in', '--MS_input_file', help='Multiband MS image file', required=True)
+parser.add_argument('-in', '--MS_input_file', help='Multiband MS image file', required=False)
 parser.add_argument('-in2', '--SWIR_input_file', help='Multiband SWIR image file for WV3', required=False)
 parser.add_argument('-out', '--output_file', help='Where NDSI image is to be saved', required=True)
 parser.add_argument('-g', '--green_band', help='Single band green channel input', required=False)
 parser.add_argument('-s3', '--swir_3_band', help='Single band SWIR input', required=False)
+parser.add_argument('-res', '--px_res', help='Pixel resolution, default is 1.2 m', required=False)
+
 
 args = parser.parse_args()
 multi_band_file = args.MS_input_file
@@ -26,15 +28,24 @@ out_fn = args.output_file
 
 green_fn=args.green_band
 s3_fn=args.swir_3_band
+px_res=args.px_res
+
+# Assign pixel resolution if no input
+if px_res is None:
+    px_res="1.2"
+    p_name="12"
+else:
+    p_name=px_res[0]+px_res[-1]
 
 # Extract reflectance as numpy arrays from proper bands (TOA or SR)    
 try:
-    with rio.open(multi_band_file[:-4] + "_b3_toa_refl.tif") as f:
+    with rio.open(multi_band_file[:-4] + "_b3_" + p_name + "_refl.tif") as f:
         green_arr=f.read()
         prf=f.profile
         g_ndv=f.nodata
 
-    with rio.open(swir_file[:-4] + "_" + multi_band_file[:-4] + "_b3_toa_refl.tif") as f:
+    with rio.open(swir_file[:-4] + "_b3_" + p_name + "_refl.tif") as f:
+#     with rio.open(swir_file[:-4] + "_" + multi_band_file[:-4] + "_b3_12_refl.tif") as f:        
         swir3_arr=f.read()
         swir3_ndv=f.nodata
 except:
@@ -49,12 +60,20 @@ except:
 
 # Calculate NDSI
 # NDSI = (green - swir) / (green + swir)
-# In ndsi.py script, NDSI is calculated with values (0, 1] for both green and SWIR bands
-ndsi_3= (green_arr - swir3_arr) / (green_arr + swir3_arr)
+ndsi_3 = (green_arr - swir3_arr) / (green_arr + swir3_arr)
+
+# Create normalized ndsi array from 0-1 for further processing
+# With min-max scaling
+ndsi_3_norm = (ndsi_3+1)/2
+
+ndsi_ndv=9999
 
 # Mask with ndv areas from green and swir arrays
-ndsi_3[green_arr==g_ndv]=g_ndv
-ndsi_3[swir3_arr==swir3_ndv]=swir3_ndv
+ndsi_3[green_arr==g_ndv]=ndsi_ndv
+ndsi_3[swir3_arr==swir3_ndv]=ndsi_ndv
+
+ndsi_3_norm[green_arr==g_ndv]=ndsi_ndv
+ndsi_3_norm[swir3_arr==swir3_ndv]=ndsi_ndv
 
 # Write these to array
 with rio.Env():
@@ -65,21 +84,6 @@ with rio.Env():
 
     with rio.open(out_fn, 'w', **prf) as dst:
         dst.write(np.squeeze(ndsi_3).astype(rio.float32), 1)
-        
-# # Do not institute low and high bounds for initial reflectance values right now.
-# # Adjust with out of bounds green and swir reflectance values
-# ndsi_3[green_arr<0]=g_ndv
-# ndsi_3[swir3_arr<0]=swir3_ndv
-# ndsi_3[green_arr>1]=g_ndv
-# ndsi_3[swir3_arr>1]=swir3_ndv
 
-# # And write to array
-# with rio.Env():
-#     # Change the band count to 1, set the
-#     # dtype to float 32, and specify LZW compression.
-#     prf.update(
-#         dtype=rio.float32,
-#         count=1,
-#         compress='lzw')
-#     with rio.open(out_fn_3_adj, 'w', **prf) as dst:
-#         dst.write(np.squeeze(ndsi_3).astype(rio.float32), 1)
+    with rio.open(out_fn[:-4]+"_minmax.tif", 'w', **prf) as dst:
+        dst.write(np.squeeze(ndsi_3_norm).astype(rio.float32), 1)
