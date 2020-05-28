b266923d796e26992fce8b759307ec42b16aae49
Michelle Hu <jmhu@uw.edu>
2020-05-27 18:57:01 -0700

    bin: updated index scripts

--- bin/ndsi_updated.py
+++ bin/ndsi_updated.py
@@ -1,89 +1,82 @@
 #!/usr/bin/env python
 
-# Calculate Normalized Difference Snow Index from WV-3 top-of-atmosphere reflectance imagery and Level 1 Landsat 8 imagery.
+# Script to calculate Normalized Difference Snow Index from input imagery (with a WV-3 MS band or single-band inputs).
+# NDSI = (green - swir) / (green + swir)
 
-import sys
 import argparse
-import struct
-
-# import third party
 import numpy as np
-from osgeo import gdal
 import rasterio as rio
 
-# Have user define input and output image filenames
-parser = argparse.ArgumentParser(description='NDSI Calculation Script with Normalized Difference Snow Index Measurement')
-parser.add_argument('-in', '--MS_input_file', help='Multiband MS image file', required=False)
-parser.add_argument('-in2', '--SWIR_input_file', help='Multiband SWIR image file for WV3', required=False)
-parser.add_argument('-out', '--output_file', help='Where NDSI image is to be saved', required=True)
-parser.add_argument('-g', '--green_band', help='Single band green channel input', required=False)
-parser.add_argument('-s3', '--swir_3_band', help='Single band SWIR input', required=False)
-parser.add_argument('-res', '--px_res', help='Pixel resolution, default is 1.2 m', required=False)
+def read_file(fn):
+    with rio.open(fn) as f:
+        arr=f.read()
+        prf=f.profile
+        ndv=f.nodata
+    return arr, prf, ndv
 
+def calc_ndsi(green_arr, swir3_arr, g_ndv=None, swir3_ndv=None):
+    # Calculate NDSI
+    ndsi_3 = (green_arr - swir3_arr) / (green_arr + swir3_arr)
 
-args = parser.parse_args()
-multi_band_file = args.MS_input_file
-swir_file = args.SWIR_input_file
-out_fn = args.output_file
+    # Create normalized ndsi array from 0-1 for further processing with min-max scaling
+    ndsi_3_norm = (ndsi_3+1)/2
+    
+    # Mask with ndv areas from original arrays
+    ndsi_3[green_arr==g_ndv]=ndsi_ndv
+    ndsi_3[swir3_arr==swir3_ndv]=ndsi_ndv
 
-green_fn=args.green_band
-s3_fn=args.swir_3_band
-px_res=args.px_res
+    ndsi_3_norm[green_arr==g_ndv]=ndsi_ndv
+    ndsi_3_norm[swir3_arr==swir3_ndv]=ndsi_ndv
+    
+    return ndsi_3, ndsi_3_norm
 
-# Assign pixel resolution if no input
-if px_res is None:
-    px_res="1.2"
-    p_name="12"
-else:
+def run(multi_band_file, swir_file, out_fn, green_fn, s3_fn, px_res, p_name):
+    if (multi_band_file is not None) & (swir_file is not None):
+        green_arr, prf, g_ndv = read_file(multi_band_file[:-4] + "_b3_" + p_name + "_refl.tif")
+        swir3_arr, _, swir3_ndv = read_file(swir_file[:-4] + "_b3_" + p_name + "_refl.tif")
+    elif (green_fn is not None) & (s3_fn is not None):
+        green_arr, prf, g_ndv = read_file(green_fn)
+        swir3_arr, _, swir3_ndv = read_file(s3_fn)
+    else:
+        sys.exit("Check input files, missing proper input")
+        break
+
+    ndsi_3, ndsi_3_norm = calc_ndsi(green_arr, swir3_arr, g_ndv, swir3_ndv)
+
+    # Write NDSI arrays to file
+    with rio.Env():
+        prf.update(
+            dtype=rio.float32,
+            count=1,
+            compress='lzw')
+        with rio.open(out_fn, 'w', **prf) as dst:
+            dst.write(np.squeeze(ndsi_3).astype(rio.float32), 1)
+        with rio.open(out_fn[:-4]+"_minmax.tif", 'w', **prf) as dst:
+            dst.write(np.squeeze(ndsi_3_norm).astype(rio.float32), 1)
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='Normalized Difference Snow Index Calculation Script')
+    parser.add_argument('-in', '--MS_input_file', help='Multiband MS image file', required=False)
+    parser.add_argument('-in2', '--SWIR_input_file', help='Multiband SWIR image file for WV3', required=False)
+    parser.add_argument('-out', '--output_file', help='Where NDSI image is to be saved', default="ndsi.tif", required=False)
+    parser.add_argument('-g', '--green_band', help='Single band green channel input', required=False)
+    parser.add_argument('-s3', '--swir_3_band', help='Single band SWIR input', required=False)
+    parser.add_argument('-res', '--px_res', help='Pixel resolution, default is 1.2 m', default="1.2", required=False)
+    return parser
+
+def main():
+    parser = get_parser
+    args = parser.parse_args()
+    in_fn = args.MS_input_file
+    swir_file = args.SWIR_input_file
+    out_fn = args.output_file
+
+    green_fn=args.green_band
+    s3_fn=args.swir_3_band
+    px_res=args.px_res
     p_name=px_res[0]+px_res[-1]
-
-# Extract reflectance as numpy arrays from proper bands (TOA or SR)    
-try:
-    with rio.open(multi_band_file[:-4] + "_b3_" + p_name + "_refl.tif") as f:
-        green_arr=f.read()
-        prf=f.profile
-        g_ndv=f.nodata
-
-    with rio.open(swir_file[:-4] + "_b3_" + p_name + "_refl.tif") as f:
-#     with rio.open(swir_file[:-4] + "_" + multi_band_file[:-4] + "_b3_12_refl.tif") as f:        
-        swir3_arr=f.read()
-        swir3_ndv=f.nodata
-except:
-    with rio.open(green_fn) as f:
-        green_arr=f.read()
-        prf=f.profile
-        g_ndv=f.nodata
-
-    with rio.open(s3_fn) as f:
-        swir3_arr=f.read()
-        swir3_ndv=f.nodata
-
-# Calculate NDSI
-# NDSI = (green - swir) / (green + swir)
-ndsi_3 = (green_arr - swir3_arr) / (green_arr + swir3_arr)
-
-# Create normalized ndsi array from 0-1 for further processing
-# With min-max scaling
-ndsi_3_norm = (ndsi_3+1)/2
-
-ndsi_ndv=9999
-
-# Mask with ndv areas from green and swir arrays
-ndsi_3[green_arr==g_ndv]=ndsi_ndv
-ndsi_3[swir3_arr==swir3_ndv]=ndsi_ndv
-
-ndsi_3_norm[green_arr==g_ndv]=ndsi_ndv
-ndsi_3_norm[swir3_arr==swir3_ndv]=ndsi_ndv
-
-# Write these to array
-with rio.Env():
-    prf.update(
-        dtype=rio.float32,
-        count=1,
-        compress='lzw')
-
-    with rio.open(out_fn, 'w', **prf) as dst:
-        dst.write(np.squeeze(ndsi_3).astype(rio.float32), 1)
-
-    with rio.open(out_fn[:-4]+"_minmax.tif", 'w', **prf) as dst:
-        dst.write(np.squeeze(ndsi_3_norm).astype(rio.float32), 1)
+    
+    run(in_fn, swir_file, out_fn, green_fn, s3_fn, px_res, p_name)
+        
+if __name__ == "__main__":    
+    main()
--- bin/ndvi_updated.py
+++ bin/ndvi_updated.py
@@ -1,82 +1,77 @@
 #!/usr/bin/env python
 
-# Calculate Normalized Difference Snow Index from WV-3 top-of-atmosphere reflectance imagery.
-
-import sys
+# Script to calculate Normalized Difference Vegetation Index from input imagery (with WV-2 and WV-3 MS band or single-band inputs).
+# NDVI = (red - nir) / (red + nir)
+    
 import argparse
-import struct
-
-# import third party
 import numpy as np
-from osgeo import gdal
 import rasterio as rio
 
-# Have user define input and output image filenames
-parser = argparse.ArgumentParser(description='NDVI Calculation Script with Normalized Difference Vegetation Index Measurement')
-parser.add_argument('-in', '--MS_input_file', help='Multiband MS image file', required=True)
-parser.add_argument('-out', '--output_file', help='Where NDVI image is to be saved', required=True)
-parser.add_argument('-r', '--red_band', help='Single band red input', required=False)
-parser.add_argument('-n', '--nir_band', help='Single band NIR channel input', required=False)
-parser.add_argument('-res', '--px_res', help='Pixel resolution, default is 1.2 m', required=False)
+def read_file(fn):
+    with rio.open(fn) as f:
+        arr=f.read()
+        prf=f.profile
+        ndv=f.nodata
+    return arr, prf, ndv
+    
+def calc_ndvi(red_arr, nir1_arr, r_ndv=None, nir1_ndv=None):
+    # Calculate NDVI
+    ndvi = (nir1_arr - red_arr) / (nir1_arr + red_arr)
 
-args = parser.parse_args()
-multi_band_file = args.MS_input_file
-out_fn = args.output_file
+    # Create normalized ndvi array from 0-1 for further processing with min-max scaling
+    ndvi_norm = (ndvi+1)/2
 
-nir1_fn=args.nir_band
-red_fn=args.red_band
-px_res=args.px_res
+    # Mask with ndv areas from original arrays
+    ndvi[red_arr==r_ndv]=r_ndv
+    ndvi[nir1_arr==nir1_ndv]=nir1_ndv
 
-if px_res is None:
-    px_res="1.2"
-    p_name="12"
-else:
+    ndvi_norm[red_arr==r_ndv]=r_ndv
+    ndvi_norm[nir1_arr==nir1_ndv]=nir1_ndv
+    
+    return ndvi, ndvi_norm
+
+def run(multi_band_file, out_fn, nir1_fn, red_fn, px_res, p_name):
+    if multi_band_file is not None:
+        red_arr, prf, r_ndv = read_file(multi_band_file[:-4] + "_b5_" + p_name + "_refl.tif")
+        nir1_arr, _, nir1_ndv = read_file(multi_band_file[:-4] + "_b7_" + p_name + "_refl.tif")
+    elif red_fn is not None:
+        red_arr, prf, r_ndv = read_file(red_fn)
+        nir1_arr, _, nir1_ndv = read_file(nir1_fn)
+            
+    ndvi, ndvi_norm = calc_ndvi(red_arr, nir1_arr, r_ndv, nir1_ndv)
+    
+    # Write NDVI arrays to file
+    with rio.Env():
+        prf.update(
+            dtype=rio.float32,
+            count=1,
+            compress='lzw')
+        with rio.open(out_fn, 'w', **prf) as dst:
+            dst.write(np.squeeze(ndvi).astype(rio.float32), 1)
+        with rio.open(out_fn[:-4]+"_minmax.tif", 'w', **prf) as dst:
+            dst.write(np.squeeze(ndvi_norm).astype(rio.float32), 1)
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='Normalized Difference Vegetation Index Calculation Script')
+    parser.add_argument('-in', '--MS_input_file', help='Multiband MS image file', required=False)
+    parser.add_argument('-out', '--output_file', help='NDVI output filename', default="ndvi.tif",  required=False)
+    parser.add_argument('-r', '--red_band', help='Single-band red input', required=False)
+    parser.add_argument('-n', '--nir_band', help='Single-band NIR channel input', required=False)
+    parser.add_argument('-res', '--px_res', help='Pixel resolution, default is 1.2m', default="1.2", required=False)
+    return parser
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+    in_fn = args.MS_input_file
+    out_fn = args.output_file
+
+    nir1_fn=args.nir_band
+    red_fn=args.red_band
+    px_res=args.px_res    
     p_name=px_res[0]+px_res[-1]
 
-# Extract reflectance as numpy arrays from proper bands (TOA or SR)    
-try:
-    with rio.open(multi_band_file[:-4] + "_b5_" + p_name + "_refl.tif") as f:
-        red_arr=f.read()
-        prf=f.profile
-        r_ndv=f.nodata
-
-    with rio.open(multi_band_file[:-4] + "_b7_" + p_name + "_refl.tif") as f:
-        nir1_arr=f.read()
-        nir1_ndv=f.nodata
-        
-except:
-    with rio.open(r_fn) as f:
-        red_arr=f.read()
-        prf=f.profile
-        r_ndv=f.nodata
-
-    with rio.open(nir1_fn) as f:
-        nir1_arr=f.read()
-        nir1_ndv=f.nodata
-
-# Calculate NDVI
-ndvi = (nir1_arr - red_arr) / (nir1_arr + red_arr)
-
-# Create normalized ndsi array from 0-1 for further processing
-# With min-max scaling
-ndvi_norm = (ndvi+1)/2
-
-# Mask with ndv areas from green and swir arrays
-ndvi[red_arr==r_ndv]=r_ndv
-ndvi[nir1_arr==nir1_ndv]=nir1_ndv
-
-ndvi_norm[red_arr==r_ndv]=r_ndv
-ndvi_norm[nir1_arr==nir1_ndv]=nir1_ndv
-
-# Write these to array
-with rio.Env():
-    prf.update(
-        dtype=rio.float32,
-        count=1,
-        compress='lzw')
-
-    with rio.open(out_fn, 'w', **prf) as dst:
-        dst.write(np.squeeze(ndvi).astype(rio.float32), 1)
-
-    with rio.open(out_fn[:-4]+"_minmax.tif", 'w', **prf) as dst:
-        dst.write(np.squeeze(ndvi_norm).astype(rio.float32), 1)
+    run(in_fn, out_fn, nir1_fn, red_fn, px_res, p_name)
+    
+if __name__ == "__main__":    
+    main()
